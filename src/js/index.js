8baf452c84bbfe00413bca6e552e65765cf616fc
Veronika Kovalenko <podaritebegemotika@gmail.com>
2022-09-12 12:34:24 +0300

    s

--- src/js/index.js
+++ src/js/index.js
@@ -46,7 +46,7 @@ load_more.addEventListener('click', () => {
 
   page += 1;
 
-  if (page < totalPages) {
+  if (page > pageSize) {
     load_more.classList.add('is-hidden');
     Notify.info("We're sorry, but you've reached the end of search results.");
   }
